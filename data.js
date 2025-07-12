7a889a3795017eb4fa380430a2bfecdc1859dfdb
sheikhwebdevloper <azraburkhani@gmail.com>
2025-07-12 16:47:01 +0530

    data.js
    Signed-off-by: sheikhwebdevloper <azraburkhani@gmail.com>

---
+++ data.js
@@ -0,0 +1,84 @@
+const studentData = () => {
+    const data = [
+        {
+            "rollNo": "101",
+            "name": "Amit Kumar",
+            "fatherName": "Rakesh Kumar",
+            "course": "B.Tech Computer Science",
+            "address": "123, Green Street, Delhi",
+            "gender": "Male",
+            "admissionYear": 2021,
+            "dob": "2003-05-14",
+            "percentage": {
+                "2021": "60%",
+                "2022": "80%",
+                "2023": "75%"
+            },
+            "result_images": {
+                "2021": "results/show-exam-results-online-sample.png",
+                "2022": "Not Issued",
+                "2023": "Not Issued"
+            }
+        },
+        {
+            "rollNo": "102",
+            "name": "Priya Sharma",
+            "fatherName": "Anil Sharma",
+            "course": "B.Com",
+            "address": "45, Park Avenue, Mumbai",
+            "gender": "Female",
+            "admissionYear": 2020,
+            "dob": "2002-08-25",
+            "percentage": {
+                "2020": "7.8 CGPA",
+                "2021": "8.1 CGPA",
+                "2022": "8.4 CGPA"
+            },
+            "result_images": {
+                "2020": "results/show-exam-results-online-sample.png",
+                "2021": "Not Issued",
+                "2022": "Not Issued"
+            }
+        },
+        {
+            "rollNo": "103",
+            "name": "Rahul Verma",
+            "fatherName": "Suresh Verma",
+            "course": "B.Sc Physics",
+            "address": "789, Lake Road, Kolkata",
+            "gender": "Male",
+            "admissionYear": 2019,
+            "dob": "2001-01-10",
+            "percentage": {
+                "2019": "7.5 CGPA",
+                "2020": "7.9 CGPA",
+                "2021": "8.0 CGPA"
+            },
+            "result_images": {
+                "2019": "Not Issued",
+                "2020": "results/show-exam-results-online-sample.png",
+                "2021": "Not Issued"
+            }
+        },
+        {
+            "rollNo": "104",
+            "name": "Anjali Mehta",
+            "fatherName": "Rajesh Mehta",
+            "course": "BA History",
+            "address": "56, Rose Garden, Jaipur",
+            "gender": "Female",
+            "admissionYear": 2022,
+            "dob": "2004-03-30",
+            "percentage": {
+                "2022": "8.6 CGPA",
+                "2023": "8.9 CGPA"
+            },
+            "result_images": {
+                "2022": "results/show-exam-results-online-sample.png",
+                "2023": "Not Issued"
+            }
+        }
+    ];
+
+    return data;
+}
